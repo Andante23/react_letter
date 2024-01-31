2e67b72b3f1f8259abfc0442df6ea4eeecd5b97d
andan <andante1104@naver.com>
2024-01-31 09:17:50 +0900

    스타일드 컴포넌트 분리 안한 부분 생겨서 해결하고 반영

--- src/configstore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import { FanLetterReducer } from "./ridux/modules/FanLetter";
-
-const rootReducer = combineReducers({
-  // 생성한 module 넣기
-  FanLetterReducer,
-});
-const store = createStore(rootReducer);
-export default store;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,23 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "pages/Home";
 import Detail from "pages/Detail";
 
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
 const Router = () => {
-  // useSelector를 이용해서 store라는  스토어에서 조회하기
-  const data = useSelector((state) => {
-    return state;
-  });
-
-  //  console.log("data", data);
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home data={data} />} />
+        <Route path="/" element={<Home />} />
 
         {/* 동적라우팅 */}
-        <Route path="detail/:id" element={<Detail data={data} />} />
+        <Route path="detail/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );

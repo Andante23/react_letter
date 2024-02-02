6ad6f5b63121f765161ff208188d0ea75d48ac3a
andan <andante1104@naver.com>
2024-02-02 15:06:17 +0900

    수정

---
+++ src/style/LogicalStyle.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+// LetterInputForm.jsx
+export const StLetterFormOption = styled.div`
+  margin-left: 300px;
+`;
+
+export const StLetterFormOptionButton = styled.button`
+  margin: 10px;
+  padding: 10px;
+  border-color: #0b69d4;
+  background-color: #0b69d4;
+  border-radius: 10px;
+  color: white;
+
+  &:hover {
+    background-color: #0680c2;
+    border-color: #0680c2;
+    cursor: pointer;
+  }
+`;
+
+export const StFilTerCardBorder = styled.div`
+  background-color: black;
+  border-radius: 5px;
+  color: white;
+  margin: 10px;
+`;
+
+export const StFilTerCardItem = styled.figure`
+  display: flex;
+  padding: 10px;
+  align-items: center;
+`;
+
+export const StFilTerCardItemHeroImage = styled.img`
+  margin-left: 10px;
+  margin-right: 12px;
+  border-radius: 10px;
+  width: 100px;
+`;
+
+export const StToThePage = styled.p`
+  margin-left: auto;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+export const StPostView = styled.div`
+  margin-left: 1600px;
+  margin-top: 40px;
+`;
+
+export const StPostViewButton = styled.button`
+  border-radius: 10px;
+  margin-left: 10px;
+`;

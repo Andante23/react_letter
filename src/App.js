5b97831b0d52d11a1b67cac86c533fbd7b141c3f
andan <andante1104@naver.com>
2024-03-17 12:08:43 +0900

    GlobalStyle 경로 App.js에 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Router from "shared/Router";
-
+import { GlobalStyle } from "./Style/GlobalStyle.jsx";
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Router />
     </>
   );
